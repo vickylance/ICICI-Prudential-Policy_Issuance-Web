e9e426f74d40a328726cafbcd7b8fa2dc59dddd0
Vignesh S <vignesh6993@live.in>
2017-01-25 17:39:28 +0530

    Working bot with ICICI Ui Changes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 var app = angular.module('submitExample', []);
 app.controller('ExampleController', ['$scope', '$http', function ($scope, $http) {
-	var messages = j('.messages-content'), d, i = 0, msg = "", botmsg = "";
+	var messages = j('.messages-content'),
+		d, i = 0,
+		msg = "",
+		botmsg = "";
 
 	function GetConversationId() {
 		console.log("get conversationId");
@@ -8,7 +11,7 @@ app.controller('ExampleController', ['$scope', '$http', function ($scope, $http)
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
-				'Authorization': 'BotConnector hd54uUzzX8A.cwA.vC4.3WYZBNcDR-9yYQ1fTbiR_B-fH3A1PD6AFiKF-l3BDvA'
+				'Authorization': 'BotConnector QiqpVfEokVI.cwA.5vE.22sCnPEo93AXDH0oESiQ9xJZR69MKklf_BleqscPR7M'
 			},
 			url: 'https://directline.botframework.com/api/conversations'
 		}).then(function (response) {
@@ -22,14 +25,14 @@ app.controller('ExampleController', ['$scope', '$http', function ($scope, $http)
 				PostMessage();
 			}, 1000);
 		});
-	}
+	};
 
 	function PostMessage() {
 		$http({
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
-				'Authorization': 'BotConnector hd54uUzzX8A.cwA.vC4.3WYZBNcDR-9yYQ1fTbiR_B-fH3A1PD6AFiKF-l3BDvA'
+				'Authorization': 'BotConnector QiqpVfEokVI.cwA.5vE.22sCnPEo93AXDH0oESiQ9xJZR69MKklf_BleqscPR7M'
 			},
 			url: 'https://directline.botframework.com/api/conversations/' + $scope.conversationId + '/messages',
 			data: {
@@ -41,32 +44,32 @@ app.controller('ExampleController', ['$scope', '$http', function ($scope, $http)
 				GetMessage();
 			}, 5000);
 		});
-	}
+	};
 
 	function GetMessage() {
 		$http({
 			method: 'GET',
 			headers: {
 				'Content-Type': 'application/json',
-				'Authorization': 'BotConnector hd54uUzzX8A.cwA.vC4.3WYZBNcDR-9yYQ1fTbiR_B-fH3A1PD6AFiKF-l3BDvA'
+				'Authorization': 'BotConnector QiqpVfEokVI.cwA.5vE.22sCnPEo93AXDH0oESiQ9xJZR69MKklf_BleqscPR7M'
 			},
 			url: 'https://directline.botframework.com/api/conversations/' + $scope.conversationId + '/messages',
 		}).success(function (response) {
 			botMessage(response["messages"][1]["text"]);
 		});
-	}
+	};
 
 	$scope.submit = function () {
 		insertMessage(j('.message-input').val());
 		if ($scope.name) {
 			GetConversationId();
 		}
-	}
+	};
 
 	j(window).load(function () {
 		messages.mCustomScrollbar();
 		setTimeout(function () {
-			botMessage("Hi! I'm your tax advisor. How may I help you.");
+			botMessage("Hi! I'm your insurance advisor. How may I help you.");
 		}, 100);
 	});
 
@@ -75,7 +78,7 @@ app.controller('ExampleController', ['$scope', '$http', function ($scope, $http)
 			scrollInertia: 10,
 			timeout: 0
 		});
-	}
+	};
 
 	function formatAMPM(date) {
 		var hours = date.getHours();
@@ -86,16 +89,16 @@ app.controller('ExampleController', ['$scope', '$http', function ($scope, $http)
 		minutes = minutes < 10 ? '0' + minutes : minutes;
 		var strTime = hours + ':' + minutes + ' ' + ampm;
 		return strTime;
-	}
+	};
 
 	function setDate(t) {
 		d = new Date();
 		j('<div class="timestamp">' + formatAMPM(d) + '</div>').appendTo(t);
-	}
+	};
 
 	function setTyping() {
 		j('<div class="timestamp">Typing...</div>').appendTo(j('.message:last'));
-	}
+	};
 
 	function insertMessage(msg) {
 		if (j.trim(msg) == '') {
@@ -106,7 +109,7 @@ app.controller('ExampleController', ['$scope', '$http', function ($scope, $http)
 		setDate(temp);
 		j('.message-input').val(null);
 		updateScrollbar();
-	}
+	};
 
 	function botMessage(botmsg) {
 		if (j('.message-input').val() != '') {
@@ -119,32 +122,28 @@ app.controller('ExampleController', ['$scope', '$http', function ($scope, $http)
 		setDate(temp);
 		updateScrollbar();
 		playSound('bing');
-	}
+	};
 
 	function playSound(filename) {
 		document.getElementById("sound").innerHTML = '<audio autoplay="autoplay"><source src="' + filename + '.mp3" type="audio/mpeg" /><source src="' + filename + '.ogg" type="audio/ogg" /><embed hidden="true" autostart="true" loop="false" src="' + filename + '.mp3" /></audio>';
-	}
+	};
 }]);
 
-app.controller('ChatTitleCtrl', ['$scope', function ($scope) {
-	$scope.maximChatbox = function () {
-		var e = document.getElementById("minim-chat");
-		e.style.display = "block";
-		var e = document.getElementById("maxi-chat");
-		e.style.display = "none";
-		var e = document.getElementById("chatbox");
-		e.style.margin = "0";
-		var e = document.getElementsByClassName("animHelpText")[0];
-		e.style.display = "none";
-	};
-	$scope.minimChatbox = function () {
-		var e = document.getElementById("minim-chat");
-		e.style.display = "none";
-		var e = document.getElementById("maxi-chat");
-		e.style.display = "block";
-		var e = document.getElementById("chatbox");
-		e.style.margin = "0 0 -53vh 0";
-		var e = document.getElementsByClassName("animHelpText")[0];
-		e.style.display = "block";
-	};
-}]);
+var setTimeoutID;
+j("#minim-chat").click(function () {
+	j("#minim-chat").css("display", "none");
+	j("#maxi-chat").css("display", "block");
+	// var height = (j(".chat").outerHeight(true) - 46) * -1;
+	// j(".chat").css("margin", "0 0 " + height + "px 0");
+	j(".chat").css("margin", "0 0 -344px 0");
+	setTimeoutID = setTimeout(function () {
+		j("#animHelpText").css("display", "block");
+	}, 1500);
+});
+j("#maxi-chat").click(function () {
+	j("#minim-chat").css("display", "block");
+	j("#maxi-chat").css("display", "none");
+	j(".chat").css("margin", "0");
+	j("#animHelpText").css("display", "none");
+	clearTimeout(setTimeoutID);
+});
